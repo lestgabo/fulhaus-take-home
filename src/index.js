d47b9b4efca6a0dd81a0d3c29f653ad141d98c59
lestgabo <lestgabo@gmail.com>
2020-11-21 23:12:41 -0800

    Install material ui -> css framework
    - add background into home page

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const theme = createMuiTheme({
+    typography: {
+        fontFamily: "'Roboto', 'Helvetica', 'Arial', 'sans-serif'",
+        fontSize: 14,
+        fontWeightLight: 300,
+        fontWeightRegular: 400,
+        fontWeightMedium: 500,
+    },
+});
+
+ReactDOM.render(
+    <ThemeProvider theme={theme}>
+        <App />
+    </ThemeProvider>,
+    document.getElementById('root')
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 
-const Home = () => (
-    <>
+const backgroundSource = '/background/fulhaus-bkg.png';
+const useStyles = makeStyles(() => ({
+    paperContainer: {
+        backgroundImage: `url(${backgroundSource})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        height: '100vh',
+    },
+}));
+
+const Home = () => {
+    const classes = useStyles();
+    return (
+        <div className={classes.paperContainer}>
         hello from home.
-    </>
-)
+        </div>
+    )
+}
+
 
 export default Home;
