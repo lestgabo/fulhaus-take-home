0ca8cee7b780de2eaa49afe1f8efc85a89d89ebc
lestgabo <lestgabo@gmail.com>
2020-11-21 22:34:13 -0800

    Install react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { Router, Route, Switch } from 'react-router-dom';
+
+import history from './utils/History';
+import Home from './views/Home';
+
+// global styles
 import './App.css';
 
 const App = () => {
-    return <div>Hello from app.</div>;
+    return (
+        <Router history={history}>
+            <div id="app">
+                <Switch>
+                    <Route path="/" exact component={Home} />
+                </Switch>
+            </div>
+        </Router>
+    );
 };
 
 export default App;
---
+++ src/views/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home = () => (
+    <>
+        hello from home.
+    </>
+)
+
+export default Home;
