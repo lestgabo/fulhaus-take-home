ea8b75f37f9a57db87a500a878243a2b2433a040
lestgabo <lestgabo@gmail.com>
2020-11-23 03:37:58 -0800

    Add welcome home in home page

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
 
 const backgroundSource = '/background/fulhaus-bkg.png';
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
     paperContainer: {
         backgroundImage: `url(${backgroundSource})`,
         backgroundRepeat: 'no-repeat',
         backgroundSize: 'cover',
         height: '100vh',
+    },
+    welcome: {
+        [theme.breakpoints.up('md')]: {
+            color: '#fff',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            width: '100%',
+            height: '100%'
+        },
     },
 }));
 
@@ -15,7 +26,9 @@ const Home = () => {
     const classes = useStyles();
     return (
         <div className={classes.paperContainer}>
-            Hello from Home
+            <div className={classes.welcome}>
+                <Typography variant='h1'>Welcome home</Typography>
+            </div>
         </div>
     )
 }
